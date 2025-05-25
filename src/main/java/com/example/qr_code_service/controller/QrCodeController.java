b593c8ece18ce77cb8bb6a3cae7af367bb350284
code-for-backend <test@test.com>
2025-05-25 16:24:42 +0530

    Create QR code generation API with configurable error correction level and robust error handling

--- src/main/java/com/example/qr_code_service/controller/QrCodeController.java
+++ src/main/java/com/example/qr_code_service/controller/QrCodeController.java
@@ -1,6 +1,7 @@
 package com.example.qr_code_service.controller;
 
 import com.example.qr_code_service.service.QRCodeService;
+import com.google.zxing.WriterException;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,17 +28,17 @@ public class QrCodeController {
     }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<?> qrCode(@RequestParam int size,@RequestParam String type) {
+    public ResponseEntity<?> qrCode(@RequestParam(required = false,defaultValue = "250") int size,@RequestParam(required = false,defaultValue = "png") String type,@RequestParam String content,@RequestParam(required = false,defaultValue = "L") String correction) {
         //Response<Entity<byte[]> not used so as to simplify error handling
         try {
-            byte[] bytes=qrCodeService.getImage(size,type);
+            byte[] bytes=qrCodeService.getImage(size,type,content,correction);
             String contentType="image/"+type;// example for content-type:image/png
             return ResponseEntity.status(200).contentType(MediaType.valueOf(contentType)).body(bytes);
 
         }
-        catch(IOException e)//line 32
+        catch(IOException| WriterException e)//line 32
             {
-               String errorMsg = "Some error occured in generating the image";
+               String errorMsg = "Some error occurred in generating the QR code on server";
                 return ResponseEntity.status(500).contentType(MediaType.APPLICATION_JSON)
                         .body(Map.of("error",errorMsg));
 
--- src/main/java/com/example/qr_code_service/service/QRCodeService.java
+++ src/main/java/com/example/qr_code_service/service/QRCodeService.java
@@ -1,5 +1,12 @@
 package com.example.qr_code_service.service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
@@ -9,26 +16,35 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class QRCodeService {
-    private static List<String> imageFormats = List.of("jpeg", "png", "gif","jpg");
+    private static final List<String> imageFormats = List.of("jpeg", "png", "gif","jpg");
+    private static final List<String> correctionLevel=List.of("L","M","Q","H");
 
-    public byte[] getImage(int size, String type) throws IllegalArgumentException, IOException {
-        if (size < 150 || size > 350)
+    public byte[] getImage(int size, String type,String content,String correction) throws IllegalArgumentException, IOException, WriterException {
+           if(content.isBlank())
+            throw new IllegalArgumentException("Contents cannot be null or blank");
+
+        else if (size < 150 || size > 350)
             throw new IllegalArgumentException("Image size must be between 150 and 350 pixels");
         else if (!imageFormats.contains(type))
             throw new IllegalArgumentException("Only png, jpeg and gif image types are supported");
+        else if(!correctionLevel.contains(correction))
+            throw new IllegalArgumentException("Permitted error correction levels are L, M, Q, H");
         else {
-            BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-            Graphics g = image.createGraphics();
-            g.setColor(Color.WHITE);//it's like picking up a white paint brush
-            g.fillRect(0, 0, size, size);
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ImageIO.write(image, type, byteArrayOutputStream);//writes the image to the byteArrayOutuptStream
-            //rather than saving it to a file in the specified format
-            byte[] bytes = byteArrayOutputStream.toByteArray();//serialize this image to send over the network
-            return bytes;
+
+               QRCodeWriter qrCodeWriter=new QRCodeWriter();
+               Map<EncodeHintType,?> errorCorrectionHints=Map.of(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.valueOf(correction));
+               BitMatrix bitMatrix=qrCodeWriter.encode(content, BarcodeFormat.QR_CODE,size,size,errorCorrectionHints);
+               BufferedImage image= MatrixToImageWriter.toBufferedImage(bitMatrix);
+               ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+               ImageIO.write(image, type, byteArrayOutputStream);//writes the image to the byteArrayOutuptStream
+               //rather than saving it to a file in the specified format
+               byte[] bytes = byteArrayOutputStream.toByteArray();//serialize this image to send over the network
+               return bytes;
+
 
 
         }
