a95ae0e813bdd5c84f71587e9d3b2e8598d70613
code-for-backend <test@test.com>
2025-05-25 13:09:04 +0530

    Update api so that it takes a size and type param for qr code and add QRCodeService class to seperate the business logic from the controller

--- src/main/java/com/example/qr_code_service/controller/QrCodeController.java
+++ src/main/java/com/example/qr_code_service/controller/QrCodeController.java
@@ -1,5 +1,6 @@
 package com.example.qr_code_service.controller;
 
+import com.example.qr_code_service.service.QRCodeService;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,29 +15,35 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+//Code tested and working
 @RestController
 public class QrCodeController {
+    private final QRCodeService qrCodeService;
+
+    public QrCodeController(QRCodeService qrCodeService)
+    {
+        this.qrCodeService=qrCodeService;
+    }
 
     @GetMapping("/api/qrcode")
-    public ResponseEntity<byte[]> qrCode() {
-        BufferedImage image = new BufferedImage(250, 250, BufferedImage.TYPE_INT_RGB);
-        Graphics g = image.createGraphics();
-        g.setColor(Color.WHITE);//it's like picking up the white paint brus
-        g.fillRect(0, 0, 250, 250);
+    public ResponseEntity<byte[]> qrCode(@RequestParam int size,@RequestParam String type) {
         try {
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            ImageIO.write(image, "png", byteArrayOutputStream);//writes the image to the byteArrayOutuptStream
-            //rather than to disk
-            byte[] bytes = byteArrayOutputStream.toByteArray();//serialize this image to send over the network
-            return ResponseEntity.status(200).contentType(MediaType.IMAGE_PNG).body(bytes);
-        }
+            byte[] bytes=qrCodeService.getImage(size,type);
+            String contentType="image/"+type;// example for content-type:image/png
+            return ResponseEntity.status(200).contentType(MediaType.valueOf(contentType)).body(bytes);
 
-        catch(IOException e)
+        }
+        catch(IOException e)//line 32
             {
-                String errorMsg = "Some error occured";
-                return ResponseEntity.status(500).contentType(MediaType.TEXT_PLAIN).body(errorMsg.getBytes());
+               // String errorMsg = "Some error occured in generating the image";
+                return ResponseEntity.status(500).contentType(MediaType.TEXT_PLAIN).body(e.getMessage().getBytes());
 
             }
+
+        catch(IllegalArgumentException e)
+        {
+            return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body(e.getMessage().getBytes());
+        }
 
 
 
---
+++ src/main/java/com/example/qr_code_service/service/QRCodeService.java
@@ -0,0 +1,39 @@
+package com.example.qr_code_service.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class QRCodeService {
+    private static List<String> imageFormats = List.of("jpeg", "png", "gif");
+
+    public byte[] getImage(int size, String type) throws IllegalArgumentException, IOException {
+        if (size < 150 || size > 350)
+            throw new IllegalArgumentException("Image size must be between 150 and 350 pixels");
+        else if (!imageFormats.contains(type))
+            throw new IllegalArgumentException("Only png, jpeg and gif image types are supported");
+        else {
+            BufferedImage image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+            Graphics g = image.createGraphics();
+            g.setColor(Color.WHITE);//it's like picking up a white paint brush
+            g.fillRect(0, 0, size, size);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+             ImageIO.write(image, "png", byteArrayOutputStream);//writes the image to the byteArrayOutuptStream
+            //rather than saving it to a file in the specified format
+            byte[] bytes = byteArrayOutputStream.toByteArray();//serialize this image to send over the network
+            return bytes;
+
+
+        }
+
+    }
+
+
+}
